84cbe1cdbcc84898e3d7233402bb732f01c5f454
Akshay Pokley <akshay85pokley@gmail.com>
2017-05-19 15:59:11 +0530

    Currently Work on Proposal Script

---
+++ src/Pages/ArchitectLogin/ApplicationSubmission/Applicationtab/BuildingDetails.java
@@ -0,0 +1,101 @@
+package Pages.ArchitectLogin.ApplicationSubmission.Applicationtab;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by laxmikant on 19/05/2017.
+ */
+public class BuildingDetails {
+    WebDriver driver;
+
+    @FindBy(xpath = "")
+    WebElement BulidingTab;
+
+    @FindBy(xpath = "")
+    WebElement ClickBuildingTab;
+
+    @FindBy(xpath = "")
+    WebElement BuildingName;
+
+    @FindBy(xpath = "")
+    WebElement BuildingUse;
+
+    @FindBy(xpath = "")
+    WebElement BuildingSubUse;
+
+    @FindBy(xpath = "")
+    WebElement BuildingType;
+
+    @FindBy(xpath = "")
+    WebElement NoofBSCNR;
+
+    @FindBy(xpath = "")
+    WebElement BuiHeight;
+
+    @FindBy(xpath = "")
+    WebElement NoFloor;
+
+    @FindBy(xpath = "")
+    WebElement BuildingStrucher;
+
+    @FindBy(xpath = "")
+    WebElement Deleterow;
+
+    @FindBy(xpath = "")
+    WebElement AddBuilding;
+
+    @FindBy(xpath = "")
+    WebElement SubmitBuliding;
+    /*---------------------------------------------------------------*/
+    /*Mesh WebElemebnt */
+
+    @FindBy(xpath = "")
+    WebElement Save;
+
+    @FindBy(xpath = "")
+    WebElement Delete;
+
+    @FindBy(xpath = "")
+    WebElement Close;
+
+    @FindBy(xpath = "")
+    WebElement SelectFName;
+
+    @FindBy(xpath = "")
+    WebElement Height;
+
+    @FindBy(xpath = "")
+    WebElement use;
+
+    @FindBy(xpath = "")
+    WebElement Subuse;
+
+    @FindBy(xpath = "")
+    WebElement ResFArea;
+
+    @FindBy(xpath = "")
+    WebElement CommanFA;
+
+    @FindBy(xpath = "")
+    WebElement IndFLA;
+
+    @FindBy(xpath = "")
+    WebElement SpeFloA;
+
+    @FindBy(xpath = "")
+    WebElement ParkingArea;
+
+    @FindBy(xpath = "")
+    WebElement ServiceArea;
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Pages/ArchitectLogin/ApplicationSubmission/d.java
@@ -0,0 +1,7 @@
+package Pages.ArchitectLogin.ApplicationSubmission;
+
+/**
+ * Created by laxmikant on 19/05/2017.
+ */
+public class d {
+}
